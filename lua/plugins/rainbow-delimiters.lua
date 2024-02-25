f7c6f4e6c3b4d4c0cb8e9e3ce7ac243d3bcbe755
moktavizen <moktavizzen@gmail.com>
2024-02-25 09:01:13 +0700

    move bracket colors to rainbow-deilimiter config

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,3 +1,17 @@
 return {
   "HiPhish/rainbow-delimiters.nvim",
+  config = function()
+    local colors = require("material.colors")
+    require("rainbow-delimiters.setup").setup({
+      define_hlgroups = function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = colors.main.red })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = colors.main.orange })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = colors.main.yellow })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = colors.main.green })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = colors.main.blue })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = colors.main.cyan })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = colors.main.purple })
+      end,
+    })
+  end,
 }
